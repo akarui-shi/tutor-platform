f874726a16075207e94a50be12ae87da3e755144
Мария Кучерова <125154747+akarui-shi@users.noreply.github.com>
2025-12-11 20:57:57 +0300

    Add CreateUserRequest DTO

---
+++ auth-service/src/main/java/ru/tutorplatform/auth/dto/CreateUserRequest.java
@@ -0,0 +1,16 @@
+package ru.tutorplatform.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+    private String email;
+    private String password;
+    private String role;
+}
