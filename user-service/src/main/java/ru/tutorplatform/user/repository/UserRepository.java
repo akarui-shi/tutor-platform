4091b571651461debb86c50dde5d9c9a7bf554af
Мария Кучерова <125154747+akarui-shi@users.noreply.github.com>
2025-12-11 21:18:59 +0300

    Add findByEmail method to UserRepository

--- user-service/src/main/java/ru/tutorplatform/user/repository/UserRepository.java
+++ user-service/src/main/java/ru/tutorplatform/user/repository/UserRepository.java
@@ -1,16 +1,13 @@
 package ru.tutorplatform.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.tutorplatform.user.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
 }
-
-
-
-
-
-
