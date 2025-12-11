9817c6e82f47e1ac3c26b8ff8114a33e07b7ebc0
Мария Кучерова <125154747+akarui-shi@users.noreply.github.com>
2025-12-11 20:57:51 +0300

    Add UserDto for auth service

---
+++ auth-service/src/main/java/ru/tutorplatform/auth/dto/UserDto.java
@@ -0,0 +1,20 @@
+package ru.tutorplatform.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String email;
+    private String username;
+    private String passwordHash;
+    private String role;
+    private String firstName;
+    private String lastName;
+}
